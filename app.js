e31d44360c332211ffe923ffb6261ced571de2c1
catsdgs <catsdgs@disr.it>
2020-09-28 18:39:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,55 +17,12 @@ var Transform = require('stream').Transform;
 
 var app = express();
 
-var google_analytics_id = process.env.GA_ID || null;
-
-function addGa(html) {
-    if (google_analytics_id) {
-        var ga = [
-            "<script type=\"text/javascript\">",
-            "var _gaq = []; // overwrite the existing one, if any",
-            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
-            "_gaq.push(['_trackPageview']);",
-            "(function() {",
-            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
-            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
-            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
-            "})();",
-            "</script>"
-            ].join("\n");
-        html = html.replace("</body>", ga + "\n\n</body>");
-    }
-    return html;
-}
-
-function googleAnalyticsMiddleware(data) {
-    if (data.contentType == 'text/html') {
-
-        // https://nodejs.org/api/stream.html#stream_transform
-        data.stream = data.stream.pipe(new Transform({
-            decodeStrings: false,
-            transform: function(chunk, encoding, next) {
-                this.push(addGa(chunk.toString()));
-                next();
-            }
-        }));
-    }
-}
-
-var unblockerConfig = {
-    prefix: '/p/',
-    responseMiddleware: [
-        googleAnalyticsMiddleware
-    ]
-};
-
-
 
 // this line must appear before any express.static calls (or anything else that sends responses)
 app.use(unblocker(unblockerConfig));
 
 // serve up static files *after* the proxy is run
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/wirkus'));
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
 app.get("/no-js", function(req, res) {
